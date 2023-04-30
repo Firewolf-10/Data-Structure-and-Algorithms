cdd135ce6e3418112c778cdda35f80cf34dcd2a6
Firewolf <dhanarajjangi18@gmail.com>
2023-05-01 01:49:22 +0530

    dynamicstack Class implemented

--- Data Structure/stacks/Main.java
+++ Data Structure/stacks/Main.java
@@ -2,14 +2,19 @@ package stacks;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        CustomStack cs = new CustomStack(5);
+        DynamicStack cs = new DynamicStack(5);
         cs.push(1);
         cs.push(2);
         cs.push(3);
         cs.push(4);
         cs.push(5);
         System.out.println(cs.peek());
-//        cs.push(6);
+        cs.push(6);
+        cs.push(7);
+        cs.push(8);
+        cs.push(9);
+        cs.push(10);
+        cs.push(11);
         cs.display();
 
         cs.pop();
